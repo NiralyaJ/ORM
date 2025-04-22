25ca69cbe857bf0821b63205e20a5a15499e8817
niralya <niralyajayabalaji@gmail.com>
2025-04-22 22:30:24 +0530

    success

--- potter/ormapp/admin.py
+++ potter/ormapp/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin
-from .models import Employee, EmployeeAdmin
-admin.site.register(Employee, EmployeeAdmin)
+from django.contrib import admin 
+from .models import Movie_DB,Movie_DBAdmin 
+admin.site.register(Movie_DB,Movie_DBAdmin)
--- potter/ormapp/models.py
+++ potter/ormapp/models.py
@@ -1,11 +1,12 @@
-from django.db import models
-from django.contrib import admin
-class Employee (models.Model):
-    eid=models.CharField(max_length=20,help_text="Employee")
-    name=models.CharField(max_length=100)
-    salary=models. IntegerField()
-    age=models.IntegerField()
-    email=models. EmailField()
+from django.db import models 
+from django.contrib import admin 
+class  Movie_DB(models.Model): 
+    Movie_ID = models.CharField(max_length=20, primary_key=True) 
+    Title = models.CharField(max_length=100) 
+    Genre = models.CharField(max_length=20) 
+    Rating = models.IntegerField( ) 
+    Language = models.CharField(max_length=15) 
+    Release_Date = models.DateField( ) 
 
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display=('eid', 'name', 'salary','age','email')
+class Movie_DBAdmin(admin.ModelAdmin): 
+    list_display = ('Movie_ID', 'Title', 'Genre', 'Rating', 'Language', 'Release_Date')
